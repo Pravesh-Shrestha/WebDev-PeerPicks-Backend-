1daf3f3a7825fb5f32dcdca28448e4c9ce7bc195
Pravesh <stharavesh12@gmail.com>
2025-01-07 14:24:16 +0545

    First Commit

---
+++ model/test.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database/db");
+
+const Test = sequelize.define("test", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Test;
