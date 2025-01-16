fb9a2862f9487e8603333e368af7e169deb16847
Pravesh <stharavesh12@gmail.com>
2025-01-16 13:03:29 +0545

    Changes made

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,22 +1,36 @@
-const express = require('express');
+// const express = require('express');
+// const router = express.Router();
+
+// // Define your routes
+// router.get('/', (req, res) => {
+//     res.send('Get all users');
+// });
+
+// router.post('/', (req, res) => {
+//     res.send('Create user');
+// });
+
+// router.put('/:id', (req, res) => {
+//     res.send(`Update user ${req.params.id}`);
+// });
+
+// router.delete('/:id', (req, res) => {
+//     res.send(`Delete user ${req.params.id}`);
+// });
+
+// // Make sure to export the router
+// module.exports = router;
+
+
+
+//Import the express library
+const express = require('express')
+
 const router = express.Router();
 
-// Define your routes
-router.get('/', (req, res) => {
-    res.send('Get all users');
-});
+const userController = require('../Controller/userController');
 
-router.post('/', (req, res) => {
-    res.send('Create user');
-});
+router.post('/create_test', userController.createTest);
+router.get('/show_test', userController.getTest);
 
-router.put('/:id', (req, res) => {
-    res.send(`Update user ${req.params.id}`);
-});
-
-router.delete('/:id', (req, res) => {
-    res.send(`Delete user ${req.params.id}`);
-});
-
-// Make sure to export the router
 module.exports = router;
