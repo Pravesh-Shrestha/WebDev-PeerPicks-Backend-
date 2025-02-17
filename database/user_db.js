e5a5d4dc7ddd52ccea8835d22533ecb5a4076806
Pravesh <stharavesh12@gmail.com>
2025-02-17 16:18:41 +0545

    Changes made in files

--- database/user_db.js
+++ database/user_db.js
@@ -1,18 +1,27 @@
-const {Sequelize} = require("sequelize");
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
 
-const sequelize = new Sequelize('PeerPicks_db','postgres','admin123',{
-    host:'localhost',
-    dialect:'postgres',
-    port:5432,
-    logging:false,
-});
+dotenv.config();
+
+const sequelize = new Sequelize(
+  'PeerPicks_db', // Database name
+  'postgres', // Username
+  'admin123', // Password
+  {
+    host: 'localhost',
+    dialect: "postgres",
+    port: 5432,
+    logging: false,
+  }
+);
 
 const connectDatabase = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connection to the database has been established successfully.");
+    console.log("✅ Connection to the database has been established successfully.");
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error("❌ Unable to connect to the database:", error);
+    process.exit(1);
   }
 };
 
